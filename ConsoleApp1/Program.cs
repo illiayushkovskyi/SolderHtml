94a2fef28c2aed867d180b66b89b7893994c4e82
Illia Yushkovskyi <haishtipil@gmail.com>
2023-11-13 19:12:30 +0100

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            using (StreamReader reader = new StreamReader("D:\\Kletki.txt"))
+            {
+                string line = null;
+                stringBuilder.Append("[");
+                while ((line = reader.ReadLine()) != null)
+                {
+                    stringBuilder.Append("[");
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        stringBuilder.Append(line[i]);
+                        if(i + 1< line.Length)
+                        {
+                            stringBuilder.Append(",");
+                        }
+                    }
+                    stringBuilder.Append("],");
+                    stringBuilder.AppendLine();
+                }
+                stringBuilder.Append("]");
+
+
+            }
+
+
+            using (StreamWriter write = new StreamWriter("D:\\Kletki1.txt"))
+            {
+                write.WriteLine(stringBuilder);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
